a8317eca147d9bc5b93ec4027f48fda225eaee35
yrkeman <114952762+yrkeman@users.noreply.github.com>
2023-02-20 02:22:29 +0000

    Add files via upload

---
+++ SimpleBMICalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBMICalcApp.swift
+//  SimpleBMICalc
+//
+//  Created by Emmanuel Begahh on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBMICalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
